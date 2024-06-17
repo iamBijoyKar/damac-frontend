e7c65ea0a12475fe19e83e94690177d7956f065f
Bijoy Kar <bijoykar54321@gmail.com>
2024-06-17 19:44:17 +0530

    styles: Tailwindcss custom theme added with css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: 'rgb(var(--primary) / <alpha-value>)',
+        secondary: 'rgb(var(--secondary) / <alpha-value>)',
+        tertiary: 'rgb(var(--tertiary) / <alpha-value>)',
+        bodyText: 'rgb(var(--body-text) / <alpha-value>)',
+        bodyText2: 'rgb(var(--body-text-2) / <alpha-value>)',
+        footerBg: 'rgb(var(--footer-bg) / <alpha-value>)',
+      },
+      fontFamily: {
+        body: ['Poppins', 'sans-serif'],
+        display: ['Oswald', 'sans-serif'],
       },
     },
   },
