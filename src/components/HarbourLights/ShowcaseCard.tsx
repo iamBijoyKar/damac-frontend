06670b8ee86401c711f7935c944af11e6f94f2f6
Bijoy Kar <bijoykar54321@gmail.com>
2024-06-19 15:18:08 +0530

    update: Showcase card hover effect added

--- src/components/HarbourLights/ShowcaseCard.tsx
+++ src/components/HarbourLights/ShowcaseCard.tsx
@@ -18,8 +18,13 @@ export default function ShowcaseCard({
   return (
     <div className="relative rounded-t-full px-1 w-fit flex flex-col items-center ">
       <div className="rounded-t-full rounded-b-xl w-[150px] h-[110px] min-[410px]:w-[172px] sm:w-[198px] min-[410px]:h-[115px] bg-slate-300 absolute top-0 left-[4px] min-[410px]:left-[8px] sm:left-[15px]"></div>
-      <div className="rounded-full w-[145px] h-[145px] min-[410px]:w-[165px] min-[410px]:h-[165px] sm:w-[190px] sm:h-[190px] overflow-hidden relative mt-1">
-        <Image src={img} alt={label1} fill />
+      <div className="rounded-full w-[145px] h-[145px] min-[410px]:w-[165px] min-[410px]:h-[165px] sm:w-[190px] sm:h-[190px] overflow-hidden relative mt-1 ">
+        <Image
+          src={img}
+          className="hover:scale-110 duration-300 transition-all ease-in-out "
+          alt={label1}
+          fill
+        />
       </div>
       <div className="p-6 w-[150px] h-[230px] min-[410px]:w-[180px] min-[410px]:h-[200px] sm:w-[220px] sm:h-[180px] rounded-b-xl shadow-lg -z-10 -mt-20">
         <div className=" mt-20">
